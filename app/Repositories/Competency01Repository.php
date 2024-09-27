cbba6b72bc00f547c690659a076bb98323939a2c
bamboozaa <chukadee@gmail.com>
2024-09-27 17:25:28 +0700

    Competency Commit

---
+++ app/Repositories/Competency01Repository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+class Competency01Repository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
